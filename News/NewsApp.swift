53b31be5c56f24c533ba61097b6c53d52f1dc594
Azhar <muh.azharudheen@gmail.com>
2024-01-09 21:16:27 +0400

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Personal on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
