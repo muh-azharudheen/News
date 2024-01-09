7431b8a4b4d2b32df29c265ad20e803986986515
Arshif1 <mohammedarshif456@gmail.com>
2024-01-09 23:15:08 +0530

    changed

--- News/ContentView.swift
+++ News/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Arshif")
+            Text("Hello, yooo")
         }
         .padding()
     }
